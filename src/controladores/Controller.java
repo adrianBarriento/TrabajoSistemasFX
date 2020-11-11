bfec30388af0d2020c5cd1108120e8e290dc60fb
Adrian Barriento <adri.bace@gmail.com>
2020-11-11 20:34:14 +0100

    Proyecto organizado en FX

---
+++ src/controladores/Controller.java
@@ -0,0 +1,13 @@
+package controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class Controller {
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+
+    }
+}
