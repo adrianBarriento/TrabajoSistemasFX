2edcefa995153f3db1f590dee540d74179fde3a4
OscarCorraliza <59883677+OscarCorraliza@users.noreply.github.com>
2020-11-17 17:22:53 +0100

    Primer cambio funcionalidad

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("./views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setTitle("ERP de SGE");
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
 
