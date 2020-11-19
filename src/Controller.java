6cf2173a69d04625decd031800bb6f43fb3a2b19
OscarCorraliza <59883677+OscarCorraliza@users.noreply.github.com>
2020-11-19 19:23:52 +0100

    controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,15 @@
 import javafx.application.Platform;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
+    public TextField id_txtLogin;
 
     public void onExitButtonClicked(MouseEvent mouseEvent) {
         Platform.exit();
+    }
+
+    public void comprobarEmpleado(MouseEvent mouseEvent){
+        String dni = id_txtLogin.getText();
     }
 }
