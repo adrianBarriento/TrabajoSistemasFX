552f8eab66045d0061c59790fca21d4e03defaac
OscarCorraliza <59883677+OscarCorraliza@users.noreply.github.com>
2020-11-19 19:21:10 +0100

    prueba

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,37 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
-
+    private double yOffset;
+    private double xOffset;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("./views/sample.fxml"));
+
+        //Metodos para poder mover la ventana
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                xOffset = mouseEvent.getSceneX();
+                yOffset = mouseEvent.getSceneY();
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                primaryStage.setX(mouseEvent.getScreenX() - xOffset);
+                primaryStage.setY(mouseEvent.getScreenY() - yOffset);
+            }
+        });
+
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setTitle("ERP de SGE");
         Scene scene = new Scene(root);
