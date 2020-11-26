8f88a6e6b4b197eb772b3000d70e450e7dd83dae
Adrian <adrian.barriento@juanxxiii.net>
2020-11-26 19:23:22 +0100

    metodos alerta y crear usuario

---
+++ src/models/Common.java
@@ -0,0 +1,27 @@
+package models;
+
+import javafx.scene.control.Alert;
+import javafx.stage.StageStyle;
+
+public class Common {
+    //constantes
+    static final String MENSAJE_ERROR="Algo no ha funcionado de manera esperada";
+    static final String MENSAJE_EXITO_INSERCION="Se ha insertado correctamente";
+
+    public void vtnAlertaError(){
+        Alert dialogoAlerta = new Alert(Alert.AlertType.ERROR);
+        dialogoAlerta.setTitle("ERROR");
+        dialogoAlerta.setHeaderText(null);
+        dialogoAlerta.setContentText(MENSAJE_ERROR);
+        dialogoAlerta.initStyle(StageStyle.UTILITY);
+        dialogoAlerta.showAndWait();
+    }
+    public void vtnMensajeExitoInsercion(){
+        Alert dialogoAlerta = new Alert(Alert.AlertType.INFORMATION);
+        dialogoAlerta.setTitle("ÉXITO");
+        dialogoAlerta.setHeaderText(null);
+        dialogoAlerta.setContentText(MENSAJE_EXITO_INSERCION);
+        dialogoAlerta.initStyle(StageStyle.UTILITY);
+        dialogoAlerta.showAndWait();
+    }
+}
