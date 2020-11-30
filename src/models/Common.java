15c2e479145500fa3474e0131abaff4e8a77f9d8
OscarCorraliza <35729190+OscarCorraliza@users.noreply.github.com>
2020-11-30 19:29:18 +0100

    Metodos para recojer la informacion de la bbdd

--- src/models/Common.java
+++ src/models/Common.java
@@ -11,6 +11,7 @@ public class Common {
     static final String MENSAJE_ERROR="Algo no ha funcionado de manera esperada";
     static final String MENSAJE_EXITO_INSERCION="Se ha insertado correctamente";
 
+    //Ventanas informativas.
     public void vtnAlertaError(){
         Alert dialogoAlerta = new Alert(Alert.AlertType.ERROR);
         dialogoAlerta.setTitle("ERROR");
@@ -28,6 +29,7 @@ public class Common {
         dialogoAlerta.showAndWait();
     }
 
+    //Metodo crear conexion base de datos
     public Connection getConexion(){
         Connection conexion=null;
         try {
@@ -39,7 +41,7 @@ public class Common {
         }
         return conexion;
     }
-    //metodo para sacar datos de un empleado de una tabla de la base ded atos
+    //metodo para sacar datos de un empleado de una tabla de la base de datos
     public ArrayList<Employe> obtenerEmpleados(){
         ArrayList<Employe> listaEmpleados = new ArrayList<>();
         Connection connection = getConexion();
@@ -62,5 +64,52 @@ public class Common {
             vtnAlertaError();
         }
         return listaEmpleados;
+    }
+
+    //metodo para sacar datos de un cliente de una tabla de la base de datos
+    public ArrayList<Clientes> obtenerClientes(){
+        ArrayList<Clientes> listaClientes = new ArrayList<>();
+        Connection connection = getConexion();
+        PreparedStatement query;
+        ResultSet datos = null;
+        try {
+            query = connection.prepareStatement("SELECT * FROM clientes");
+            datos = query.executeQuery();
+            while(datos.next()){
+                String nombre = datos.getString(2);
+                String apellidos = datos.getString(3);
+                String direccion = datos.getString(4);
+                String poblacion = datos.getString(5);
+                String email = datos.getString(6);
+
+                Clientes clientes = new Clientes(nombre, apellidos, direccion, poblacion, email);
+                listaClientes.add(clientes);
+            }
+        } catch (SQLException e) {
+            vtnAlertaError();
+        }
+        return listaClientes;
+    }
+
+    //metodo para sacar datos de un empleado de una tabla de la base ded atos
+    public ArrayList<Proveedores> obtenerProveedores(){
+        ArrayList<Proveedores> listaProveedores = new ArrayList<>();
+        Connection connection = getConexion();
+        PreparedStatement query;
+        ResultSet datos = null;
+        try {
+            query = connection.prepareStatement("SELECT * FROM proveedores");
+            datos = query.executeQuery();
+            while(datos.next()){
+                String nombre = datos.getString(2);
+                String articulo = datos.getString(3);
+
+                Proveedores proveedores = new Proveedores(nombre, articulo);
+                listaProveedores.add(proveedores);
+            }
+        } catch (SQLException e) {
+            vtnAlertaError();
+        }
+        return listaProveedores;
     }
 }
