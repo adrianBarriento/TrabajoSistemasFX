7dcc38037d5d70d797db49a76370d51e4cb1903c
Adrian <adrian.barriento@juanxxiii.net>
2021-01-26 16:34:52 +0100

    p

--- src/views/ElegirEscandallo.java
+++ src/views/ElegirEscandallo.java
@@ -1,13 +1,31 @@
 package views;
 
 import models.Common;
+import models.Employe;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class ElegirEscandallo {
     Common common = new Common();
     public int getIdEscandallo(){
         Connection conexion = common.getConexion();
+        Statement query;
+        ResultSet datos=null;
+        int id = 0;
+        try {
+            query = conexion.createStatement();
+            datos = query.executeQuery("select MAX(id_escandallo) from `escandallo`");
 
+            while(datos.next()){
+                id = datos.getInt(1);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return id+1;
     }
+
+
+
+
 }
